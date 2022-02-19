a62cd6208ef11ff71ba4271cb4e7a3f96ced78f6
Thibaut Monin <Thibaut.Monin@diiage.org>
2022-02-19 15:35:51 +0000

    Updated auto-pull-requests.ps1

--- auto-pull-requests.ps1
+++ auto-pull-requests.ps1
@@ -1,14 +1,9 @@
-# construct base URLs
-#$apisUrl = "$($env:SYSTEM_TEAMFOUNDATIONCOLLECTIONURI)/$($env:SYSTEM_TEAMPROJECT)/_apis"
-#$projectUrl = "$apisUrl/git/repositories/sensostatvue"
 # Variables
 $organization = "$env:SYSTEM_TEAMFOUNDATIONCOLLECTIONURI"
 $project = "$env:SYSTEM_TEAMPROJECT"
 $repositoryId = "sensostatvue"
 
-Write-Output "$organization/$project/$repositoryId"
-
-# create common headers
+# Creating a HEADER
 $headers = @{}
 $headers.Add("Authorization", "Bearer $env:SYSTEM_ACCESSTOKEN")
 $headers.Add("Content-Type", "application/json")
@@ -19,7 +14,7 @@ $targetBranch = "master"
 
 # Create a Pull Request
 $pullRequestUrl = "$organization$project/_apis/git/repositories/$repositoryId/pullrequests?api-version=7.1-preview.1"
-Write-Output "$pullRequestUrl"
+
 $pullRequest = @{
         "sourceRefName" = "refs/heads/$sourceBranch"
         "targetRefName" = "refs/heads/$targetBranch"
@@ -28,20 +23,21 @@ $pullRequest = @{
     }
 
 $pullRequestJson = ($pullRequest | ConvertTo-Json -Depth 5)
+Write-Output $pullRequestJson
 
-Write-Output "Sending a REST call to create a new pull request from $sourceBranch to $targetBranch"
+Write-Output "Creating a new pull request from $sourceBranch to $targetBranch"
 
 # REST call to create a Pull Request
 
-$pullRequestResult = Invoke-RestMethod -Method POST -Headers $headers -Body $pullRequestJson -Uri $pullRequestUrl;
-$pullRequestId = $pullRequestResult.pullRequestId
+$pullRequestCall = Invoke-RestMethod -Method POST -Headers $headers -Body $pullRequestJson -Uri $pullRequestUrl;
+$pullRequestId = $pullRequestCall.pullRequestId
 
 Write-Output "Pull request created. Pull Request Id : $pullRequestId"
 
 # Set PR to auto-complete
 $setAutoComplete = @{
     "autoCompleteSetBy" = @{
-        "id" = $pullRequestResult.createdBy.id
+        "id" = $pullRequestCall.createdBy.id
     }
     "completionOptions" = @{       
         "deleteSourceBranch" = $False
