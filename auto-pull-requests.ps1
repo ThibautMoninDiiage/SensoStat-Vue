7dff48555a734e7d31a597ef60a649e866e0427a
Thibaut Monin <Thibaut.Monin@diiage.org>
2022-02-19 16:07:39 +0000

    Updated auto-pull-requests.ps1

--- auto-pull-requests.ps1
+++ auto-pull-requests.ps1
@@ -32,9 +32,6 @@ Write-Output "Creating a new pull request from $sourceBranch to $targetBranch"
 $pullRequestCall = Invoke-RestMethod -Method POST -Headers $headers -Body $pullRequestJson -Uri $pullRequestUrl;
 $pullRequestId = $pullRequestCall.pullRequestId
 
-$response = ($pullRequestCall | ConvertTo-Json -Depth 5)
-Write-Output $response
-
 Write-Output "Pull request created. Pull Request Id : $pullRequestId"
 
 # Set PR to auto-complete
@@ -53,7 +50,7 @@ $setAutoCompleteJson = ($setAutoComplete | ConvertTo-Json -Depth 5)
 Write-Output "Sending a REST call to set auto-complete on the newly created pull request"
 
 # REST call to set auto-complete on Pull Request
-$pullRequestUpdateUrl = ($projectUrl + '/pullRequests/' + $pullRequestId + '?api-version=5.1')
+$pullRequestUpdateUrl = "$organization$project/_apis/git/repositories/$repositoryId/pullrequests/$pullRequestId?api-version=7.1-preview.1"
 
 $setAutoCompleteResult = Invoke-RestMethod -Method PATCH -Headers $headers -Body $setAutoCompleteJson -Uri $pullRequestUpdateUrl
 
