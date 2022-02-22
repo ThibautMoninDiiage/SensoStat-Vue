b65cb72f1602058dc6e849fbfcd4f2a38138e5b2
ThibautMoninDev <device.thibaut@outlook.com>
2022-02-22 10:05:16 +0100

    HOTFIX

--- src/main.js
+++ src/main.js
@@ -1,8 +1,6 @@
 import { createApp } from "vue";
 import router from "./router";
-
 import App from './views/App.vue'
 import './registerServiceWorker'
-import router from './router'
 
 createApp(App).use(router).mount('#app')
