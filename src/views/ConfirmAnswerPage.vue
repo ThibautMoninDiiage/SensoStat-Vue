83456b133880dbe176d204aa6fe37b07e5255ca0
Baptiste RAMEAU <brameau@deliverup.com>
2022-02-22 09:43:13 +0100

    feat: [ConfirmAnswerPage] design ConfirmAnswerPage

---
+++ src/views/ConfirmAnswerPage.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="mainContainer">
+    <div id="microphoneContainerRetour">
+      <MainButton class="itemCentered" id="btnRetour" message="Retour" />
+      <div id="iconText">
+        <i class="fa-solid fa-microphone"></i>
+        <MicrophoneText class="itemCentered" message="Reformuler" />
+      </div>
+    </div>
+
+    <div class="title">C'est à vous !</div>
+
+    <textarea class="areaAnswer" rows="15" cols="30"></textarea>
+
+    <div></div>
+
+    <div id="microphoneContainer">
+      <MainButton class="itemCentered" message="Étape suivante" />
+      <div id="iconText">
+        <i class="fa-solid fa-microphone"></i>
+        <MicrophoneText class="itemCentered" message="Étape suivante" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+@import "../../public/styles/confirmAnswerPage.css";
+</style>
+
+<script>
+import router from "../router/index";
+import MainButton from "../components/MainButton.vue";
+import MicrophoneText from "../components/MicrophoneText.vue";
+
+export default {
+  name: "ConfirmAnswerPage",
+  components: {
+    MainButton,
+    MicrophoneText,
+  },
+};
+</script>
