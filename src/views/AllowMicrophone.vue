cd8f2a20d70995394ff29f8ddc4e9ddf35fb959f
ThibautMoninDev <device.thibaut@outlook.com>
2022-02-23 14:53:56 +0100

    Feat : (AllowMicrophone) Add allow microphone in first page

---
+++ src/views/AllowMicrophone.vue
@@ -0,0 +1,23 @@
+<template>
+    <div id="mainContainer">
+        <h1>Pour le bon fonctionnement de l'application, nous avons besoin d'utiliser votre microphone et votre sortie audio.</h1>
+        <MainButton @click="startPage" message="Autoriser"/>
+    </div>
+</template>
+
+<script>
+    import router from '../router/index'
+    import MainButton from '../components/MainButton.vue'
+
+    export default {
+        name : 'AllowMicrophone',
+        components : {
+            MainButton
+        },
+        methods : {
+            startPage() {
+                router.push('/startPage')
+            }
+        }
+    }
+</script>
