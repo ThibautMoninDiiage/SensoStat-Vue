27601b5eaccd96a1da7091d91a0d17d5c31717b6
ThibautMonin <thibaut.monin@diiage.org>
2022-03-04 14:03:03 +0100

    Ajout du bouton sur le NotAllowed

--- src/components/NotAllowed.vue
+++ src/components/NotAllowed.vue
@@ -1,11 +1,19 @@
 <template>
     <div id="mainContainer">
         <h1>Vous n'êtes pas autorisé à aller sur cette page depuis cet endroit.</h1>
+        <button @click="goBack()" class="orangeRoundedButton">Retour</button>
     </div>
 </template>
 
 <script>
+    import router from '../router/index'
+
     export default {
-        name : 'NotAllowed'
+        name : 'NotAllowed',
+        methods : {
+            goBack(){
+                router.back()
+            }
+        }
     }
 </script>
