6245949a5a8b52998a93a6a841ab09c7f53e5ff1
ThibautMonin <thibaut.monin@diiage.org>
2022-03-04 13:33:50 +0100

    Fix / Feat
    Déplacement des composants NotFound et NotAllowed dans components.
    
    Ajout de la première version de navigation guars

---
+++ src/components/NotAllowed.vue
@@ -0,0 +1,11 @@
+<template>
+    <div id="mainContainer">
+        <h1>Vous n'êtes pas autorisé à aller sur cette page depuis cet endroit.</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name : 'NotAllowed'
+    }
+</script>
