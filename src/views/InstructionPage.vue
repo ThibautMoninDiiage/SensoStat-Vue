ccfeb8e1728d568e9c72bdba090bec76adb3c921
Baptiste RAMEAU <baptiste.rameau@diiage.org>
2022-03-11 09:44:27 +0100

    fix: [Vue Pages] call data from API

--- src/views/InstructionPage.vue
+++ src/views/InstructionPage.vue
@@ -20,7 +20,7 @@ import MainButton from "../components/MainButton.vue";
 import MicrophoneText from "../components/MicrophoneText.vue";
 import TextToSpeechService from "../services/textToSpeechService";
 import SpeechToTextService from "../services/speechToTextService";
-import InstructionService from "../services/instructionService";
+import SurveyService from "../services/surveyService";
 
 export default {
   name: "InstructionPage",
@@ -36,17 +36,28 @@ export default {
       text: undefined,
       TTSService: new TextToSpeechService(),
       STTService: new SpeechToTextService(),
-      instructionService: undefined,
+      surveyService: undefined,
+      surveys: [],
       instructions: [],
+      questions: [],
+      surveyState: undefined,
     };
   },
   async mounted() {
     this.position = this.$route.params.position;
-    this.instructionService = new InstructionService();
-    var result = await this.instructionService.getInstructions();
-    this.instructions = result;
+    // CALL SURVEY SERVICE
+    this.surveyService = new SurveyService();
+    var result = await this.surveyService.getSurvey();
+    // GET SURVEYS
+    this.surveys = result;
+    // GET INSTRUCTIONS
+    this.instructions = result.instructions;
+    // GET QUESTIONS
+    this.questions = result.questions;
+    // GET SURVEY STATE
+    this.surveyState = result.surveyState.libelle;
     this.vocalCommand = 'Cliquez sur le bouton, ou dites "Suivant"';
-    this.productNumber = result[0].surveyId;
+    this.productNumber = result.id;
 
     // await this.TTSService.textToSpeech(this.instruction + this.productNumber);
     await this.TTSService.textToSpeech(
