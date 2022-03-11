51a51e989168c918baffac732ef56669c6c8780a
Baptiste RAMEAU <baptiste.rameau@diiage.org>
2022-03-11 09:43:29 +0100

    fix: [surveyService] modify Call API

--- src/services/surveyService.js
+++ src/services/surveyService.js
@@ -4,7 +4,7 @@ export default class SurveyService {
   async getSurveys() {
     return new Promise((resolve) => {
       axios
-        .get("https://appsensostatapi.azurewebsites.net/Survey", {
+        .get("https://appsensostatapi.azurewebsites.net/Survey?surveyId=54", {
           headers: {
             "Content-Type": "application/json",
             Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjEiLCJmYW1pbHlfbmFtZSI6InBsb3AiLCJqdGkiOiI5ZDNkZjEwYy1iZDFkLTQ1ZTMtYjM3ZC02OGZkZjc3ZDhjZTYiLCJuYmYiOjE2NDY4MjM2OTQsImV4cCI6MTY0NzQyODQ5MywiaWF0IjoxNjQ2ODIzNjk0LCJpc3MiOiJodHRwczovL2xvY2FsaG9zdDo3MDE5IiwiYXVkIjoiU2Vuc29TdGF0V2ViLkFwaSJ9.hrHWntXxISmKFsgBzB6pJ96rVBw_vlpbuQ4jOVITvIs`,
