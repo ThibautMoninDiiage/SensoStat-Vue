dfa14164c9d4c0d9f5cdb8fb7d6689dfa4ad8e12
ThibautMonin <thibaut.monin@diiage.org>
2022-03-24 13:38:20 +0100

    AnswerService

--- src/services/answerService.js
+++ src/services/answerService.js
@@ -2,8 +2,14 @@ import axios from 'axios'
 
 export default class AnswerService {
 
-    async saveUserAnswer() {
-        return axios.post('')
+    async saveUserAnswer(userAnswer, questionId, token, productId) {
+        await axios.post('https://appsensostatapi.azurewebsites.net/Answer',
+        {
+            userAnswer : userAnswer,
+            questionId : questionId,
+            token : token,
+            productId : productId
+        })
     }
 
 }
